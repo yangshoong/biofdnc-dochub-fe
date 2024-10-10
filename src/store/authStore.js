228c467d212070e2699b23aa124dcd7549bc5342
Yangshoong <crinaeae83@gmail.com>
2024-10-10 13:51:39 +0900

    authStore 수정

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
-  user: null,
+  user: null, // 초기 상태를 null로 설정
   setUser: (user) => set({ user }),
   logout: () => set({ user: null }),
 }));
