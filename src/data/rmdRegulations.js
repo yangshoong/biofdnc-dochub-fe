188f8516e351eecdffc3664e2c45c2f9566db6e0
Yangshoong <crinaeae83@gmail.com>
2024-10-10 13:50:53 +0900

    원료제조팀 규정 추가 중

--- src/data/rmdRegulations.js
+++ src/data/rmdRegulations.js
@@ -6,17 +6,38 @@ const rmdRegulations = [
     items: [
       { id: 'BF-RMD-GM-01', title: '업무매뉴얼', content: null }, // content를 null로 설정
       { id: 'BF-RMD-GM-02', title: '업무분장 규정', content: '...' },
-      // 나머지 소항목들...
+      { id: 'BF-RMD-GM-03', title: '기록 관리 규정', content: '...' },
+      { id: 'BF-RMD-GM-04', title: '교육훈련 규정', content: '...' },
+      { id: 'BF-RMD-GM-05', title: '제품 식별 및 추적성 관리 규정', content: '...' },
+      { id: 'BF-RMD-GM-06', title: '내부감사 규정', content: '...' },
+      { id: 'BF-RMD-GM-07', title: '문서 및 자료관리 규정', content: '...' },
+      { id: 'BF-RMD-GM-08', title: '불만 처리 규정', content: '...' },
+      { id: 'BF-RMD-GM-09', title: '위수탁생산 및 서비스관리 규정', content: '...' },
+      { id: 'BF-RMD-GM-10', title: '폐기물 처리 및 관리 규정', content: '...' },
+      { id: 'BF-RMD-GM-11', title: '회수반품처리규정', content: '...' },
     ],
   },
   {
     category: '제조위생관리기준서',
     items: [
-      { id: 'BF-RMD-HM-01', title: '작업위생 관리 규정', content: '...' },
-      // 나머지 소항목들...
+      { id: 'BF-RMD-HM-01', title: '방충 및 방서 관리 규정', content: '...' },
+    
     ],
   },
-  // 다른 대항목들...
+  {
+    category: '품질관리기준서',
+    items: [
+      { id: 'BF-RMD-QC-01', title: '검체채취 보관 규정', content: '...' },
+    
+    ],
+  },
+  {
+    category: '제조관리기준서',
+    items: [
+      { id: 'BF-RMD-PM-01', title: '작업위생 관리 규정', content: '...' },
+      // 나머지 소항목들...
+    ],
+  }
 ];
 
 export default rmdRegulations;
